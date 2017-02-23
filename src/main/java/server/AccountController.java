c0caea9d333549e535e6b0806dedfff11190d1ed
J0kerPanda <tochka313@gmail.com>
2017-02-23 13:59:35 +0300

    Added CORS Small fixes

--- src/main/java/server/AccountController.java
+++ src/main/java/server/AccountController.java
@@ -6,15 +6,13 @@ import org.jetbrains.annotations.NotNull;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpSession;
 import java.util.HashMap;
 
 @RestController
+@CrossOrigin( origins = "http://vstaem.herokuapp.com/" )
 public class AccountController {
 
     private static final String ID_ATTR = "userId";
